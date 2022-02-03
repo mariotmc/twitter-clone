06d2036a0c228500a01bd073e60960546f94a392
mariotmc <mariotomicx@gmail.com>
2022-02-03 04:50:32 +0100

    Create Email sign up button

---
+++ src/components/LoginScreen/utility/EmailSignupButton.js
@@ -0,0 +1,9 @@
+export const EmailSignupButton = () => {
+  return (
+    <>
+      <button id="email-signup-button" className="button">
+        Sign up with email
+      </button>
+    </>
+  );
+};
