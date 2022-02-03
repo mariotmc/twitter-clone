f0f28ca10f963aca8556af898c8e2a3ba03132c2
mariotmc <mariotomicx@gmail.com>
2022-02-03 04:50:56 +0100

    Create visual line break component

---
+++ src/components/LoginScreen/utility/Linebreak.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Linebreak = () => {
+  return (
+    <div id="line-break">
+      <div className="vertical-line"></div>
+      <span>or</span>
+      <div className="vertical-line"></div>
+    </div>
+  );
+};
