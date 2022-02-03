0320b792377ecf04f54a76babc7eae1293903f6e
mariotmc <mariotomicx@gmail.com>
2022-02-03 04:49:34 +0100

    Create login nav bar

---
+++ src/components/LoginScreen/LoginNav.js
@@ -0,0 +1,102 @@
+const LoginNav = () => {
+  return (
+    <nav id="login-nav">
+      <ul>
+        <li>
+          <a href="https://about.twitter.com/en" target="_blank">
+            About
+          </a>{" "}
+        </li>
+        <li>
+          <a href="https://help.twitter.com/en" target="_blank">
+            Help Center
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/en/tos" target="_blank">
+            Terms Of Service
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/en/privacy" target="_blank">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies" target="_blank">
+            Cookie Policy
+          </a>
+        </li>
+        <li>
+          <a href="https://help.twitter.com/en/resources/accessibility" target="_blank">
+            Accessibility
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo"
+            target="_blank"
+          >
+            Ads info
+          </a>
+        </li>
+        <li>
+          <a href="https://blog.twitter.com/" target="_blank">
+            Blog
+          </a>
+        </li>
+        <li>
+          <a href="https://status.twitterstat.us/" target="_blank">
+            Status
+          </a>
+        </li>
+        <li>
+          <a href="https://careers.twitter.com/" target="_blank">
+            Careers
+          </a>
+        </li>
+        <li>
+          <a href="https://about.twitter.com/en/who-we-are/brand-toolkit" target="_blank">
+            Brand Resources
+          </a>
+        </li>
+        <li>
+          <a href="https://ads.twitter.com/login?ref=gl-tw-tw-twitter-advertise" target="_blank">
+            Advertising
+          </a>
+        </li>
+        <li>
+          <a href="https://marketing.twitter.com/en" target="_blank">
+            Marketing
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=twitterforbusiness"
+            target="_blank"
+          >
+            Twitter for Business
+          </a>
+        </li>
+        <li>
+          <a href="https://developer.twitter.com/en" target="_blank">
+            Developers
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/i/directory/profiles" target="_blank">
+            Directory
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/settings/account/personalization" target="_blank">
+            Settings
+          </a>
+        </li>
+        <li>&copy; 2022 Twitter, Inc.</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default LoginNav;
