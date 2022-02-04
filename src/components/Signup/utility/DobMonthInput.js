edfd9243a18710e7ad50d3f2f7af4ec34a0f2f25
mariotmc <mariotomicx@gmail.com>
2022-02-04 12:10:42 +0100

    Create dob month input component

---
+++ src/components/Signup/utility/DobMonthInput.js
@@ -0,0 +1,51 @@
+import { ArrowDownSymbol } from "./ArrowDownSymbol";
+
+export const DobMonthInput = () => {
+  return (
+    <div id="dob-month">
+      <label className="dob-input-label" htmlFor="month">
+        Month
+      </label>
+      <select name="month" id="month" className="dob-input">
+        <option className="month-option" disabled selected value></option>
+        <option className="month-option" value="1">
+          January
+        </option>
+        <option className="month-option" value="2">
+          February
+        </option>
+        <option className="month-option" value="3">
+          March
+        </option>
+        <option className="month-option" value="4">
+          April
+        </option>
+        <option className="month-option" value="5">
+          May
+        </option>
+        <option className="month-option" value="6">
+          June
+        </option>
+        <option className="month-option" value="7">
+          July
+        </option>
+        <option className="month-option" value="8">
+          August
+        </option>
+        <option className="month-option" value="9">
+          September
+        </option>
+        <option className="month-option" value="10">
+          October
+        </option>
+        <option className="month-option" value="11">
+          November
+        </option>
+        <option className="month-option" value="12">
+          December
+        </option>
+      </select>
+      <ArrowDownSymbol class="white input-arrow-down-symbol" width="22.5px" height="22.5px" />
+    </div>
+  );
+};
