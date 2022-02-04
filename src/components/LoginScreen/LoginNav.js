8fc771965d415b77c6eb5a3d86544040d2a8e48d
mariotmc <mariotomicx@gmail.com>
2022-02-04 12:07:18 +0100

    Add noreferrer to links

--- src/components/LoginScreen/LoginNav.js
+++ src/components/LoginScreen/LoginNav.js
@@ -3,32 +3,36 @@ const LoginNav = () => {
     <nav id="login-nav">
       <ul>
         <li>
-          <a href="https://about.twitter.com/en" target="_blank">
+          <a href="https://about.twitter.com/en" target="_blank" rel="noreferrer">
             About
           </a>{" "}
         </li>
         <li>
-          <a href="https://help.twitter.com/en" target="_blank">
+          <a href="https://help.twitter.com/en" target="_blank" rel="noreferrer">
             Help Center
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/en/tos" target="_blank">
+          <a href="https://twitter.com/en/tos" target="_blank" rel="noreferrer">
             Terms Of Service
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/en/privacy" target="_blank">
+          <a href="https://twitter.com/en/privacy" target="_blank" rel="noreferrer">
             Privacy Policy
           </a>
         </li>
         <li>
-          <a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies" target="_blank">
+          <a
+            href="https://help.twitter.com/en/rules-and-policies/twitter-cookies"
+            target="_blank"
+            rel="noreferrer"
+          >
             Cookie Policy
           </a>
         </li>
         <li>
-          <a href="https://help.twitter.com/en/resources/accessibility" target="_blank">
+          <a href="https://help.twitter.com/en/resources/accessibility" target="_blank" rel="noreferrer">
             Accessibility
           </a>
         </li>
@@ -36,37 +40,42 @@ const LoginNav = () => {
           <a
             href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo"
             target="_blank"
+            rel="noreferrer"
           >
             Ads info
           </a>
         </li>
         <li>
-          <a href="https://blog.twitter.com/" target="_blank">
+          <a href="https://blog.twitter.com/" target="_blank" rel="noreferrer">
             Blog
           </a>
         </li>
         <li>
-          <a href="https://status.twitterstat.us/" target="_blank">
+          <a href="https://status.twitterstat.us/" target="_blank" rel="noreferrer">
             Status
           </a>
         </li>
         <li>
-          <a href="https://careers.twitter.com/" target="_blank">
+          <a href="https://careers.twitter.com/" target="_blank" rel="noreferrer">
             Careers
           </a>
         </li>
         <li>
-          <a href="https://about.twitter.com/en/who-we-are/brand-toolkit" target="_blank">
+          <a href="https://about.twitter.com/en/who-we-are/brand-toolkit" target="_blank" rel="noreferrer">
             Brand Resources
           </a>
         </li>
         <li>
-          <a href="https://ads.twitter.com/login?ref=gl-tw-tw-twitter-advertise" target="_blank">
+          <a
+            href="https://ads.twitter.com/login?ref=gl-tw-tw-twitter-advertise"
+            target="_blank"
+            rel="noreferrer"
+          >
             Advertising
           </a>
         </li>
         <li>
-          <a href="https://marketing.twitter.com/en" target="_blank">
+          <a href="https://marketing.twitter.com/en" target="_blank" rel="noreferrer">
             Marketing
           </a>
         </li>
@@ -74,22 +83,23 @@ const LoginNav = () => {
           <a
             href="https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=twitterforbusiness"
             target="_blank"
+            rel="noreferrer"
           >
             Twitter for Business
           </a>
         </li>
         <li>
-          <a href="https://developer.twitter.com/en" target="_blank">
+          <a href="https://developer.twitter.com/en" target="_blank" rel="noreferrer">
             Developers
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/i/directory/profiles" target="_blank">
+          <a href="https://twitter.com/i/directory/profiles" target="_blank" rel="noreferrer">
             Directory
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/settings/account/personalization" target="_blank">
+          <a href="https://twitter.com/settings/account/personalization" target="_blank" rel="noreferrer">
             Settings
           </a>
         </li>
