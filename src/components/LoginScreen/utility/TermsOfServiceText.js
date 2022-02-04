c7f421199a4ede5b6f669488ffd8b0c3da26152f
mariotmc <mariotomicx@gmail.com>
2022-02-04 12:07:39 +0100

    Add noreferrer to links

--- src/components/LoginScreen/utility/TermsOfServiceText.js
+++ src/components/LoginScreen/utility/TermsOfServiceText.js
@@ -2,11 +2,11 @@ export const TermsOfServiceText = () => {
   return (
     <small id="signup-tos">
       By signing up, you agree to the{" "}
-      <a className="tos-links" href="https://twitter.com/en/tos" target="_blank">
+      <a className="tos-links" href="https://twitter.com/en/tos" target="_blank" rel="noreferrer">
         Terms of Service
       </a>{" "}
       and{" "}
-      <a className="tos-links" href="https://twitter.com/en/privacy" target="_blank">
+      <a className="tos-links" href="https://twitter.com/en/privacy" target="_blank" rel="noreferrer">
         Privacy Policy
       </a>
       , including{" "}
@@ -14,6 +14,7 @@ export const TermsOfServiceText = () => {
         className="tos-links"
         href="https://help.twitter.com/en/rules-and-policies/twitter-cookies"
         target="_blank"
+        rel="noreferrer"
       >
         Cookie Use
       </a>
