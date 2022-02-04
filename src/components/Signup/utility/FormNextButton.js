68c6954c3b48305f0e395299ae8944a73dead4f5
mariotmc <mariotomicx@gmail.com>
2022-02-04 12:08:32 +0100

    Create form button

---
+++ src/components/Signup/utility/FormNextButton.js
@@ -0,0 +1,9 @@
+export const FormNextButton = () => {
+  return (
+    <div id="signup-next-button-container">
+      <button id="signup-next-button" className="button">
+        Next
+      </button>
+    </div>
+  );
+};
