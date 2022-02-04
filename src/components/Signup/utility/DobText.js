8457aa285fab6ac6f7a6111735ef688c25203db5
mariotmc <mariotomicx@gmail.com>
2022-02-04 12:10:00 +0100

    Create dob text component

---
+++ src/components/Signup/utility/DobText.js
@@ -0,0 +1,11 @@
+export const DobText = () => {
+  return (
+    <>
+      <p id="dob-heading">Date of birth</p>
+      <p id="dob-text">
+        This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet,
+        or something else.
+      </p>
+    </>
+  );
+};
