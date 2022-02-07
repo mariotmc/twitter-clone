777a262adbd84d2f7dd5e77874e8aea33fc60a75
mariotmc <mariotomicx@gmail.com>
2022-02-07 06:21:04 +0100

    Remove props, add state context & signup modal component

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { StateContext } from "../../App";
 import LoginBackground from "./LoginBackground";
 import LoginCTA from "./LoginCTA";
 import LoginNav from "./LoginNav";
+import SignupModal from "../Signup/SignupModal";
 
 const LoginScreen = () => {
+  const { showSignupModal } = useContext(StateContext);
+
   return (
     <>
       <div className="wrapper">
@@ -10,6 +15,7 @@ const LoginScreen = () => {
         <LoginCTA />
       </div>
       <LoginNav />
+      {showSignupModal && <SignupModal />}
     </>
   );
 };
