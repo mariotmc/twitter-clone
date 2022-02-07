3ae4d273718bcc2d9feb985226ad1aaf983e63ab
mariotmc <mariotomicx@gmail.com>
2022-02-07 06:13:44 +0100

    Add MUI tooltip component

--- src/components/Signup/utility/PhoneAlternative.js
+++ src/components/Signup/utility/PhoneAlternative.js
@@ -1,3 +1,9 @@
+import { Tooltip } from "@mui/material";
+
 export const PhoneAlternative = () => {
-  return <p id="signup-phone-alternative">Use phone instead</p>;
+  return (
+    <Tooltip title="Currently unavailable" placement="right" arrow>
+      <p id="signup-phone-alternative">Use phone instead</p>
+    </Tooltip>
+  );
 };
