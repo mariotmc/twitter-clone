6db025b0675d1a85b3f80870897ed99420e900c7
mariotmc <mariotomicx@gmail.com>
2022-02-07 06:28:52 +0100

    Restructure component

--- src/components/Signup/SignupModal.js
+++ src/components/Signup/SignupModal.js
@@ -1,16 +1,10 @@
 import { SignupForm } from "./SignupForm";
-import { ModalHead } from "./utility/ModalHead";
-import { FormNextButton } from "./utility/FormNextButton";
 
 const SignupModal = () => {
   return (
     <div id="signup-modal-container">
       <div id="signup-modal">
-        <ModalHead />
-
         <SignupForm />
-
-        <FormNextButton />
       </div>
     </div>
   );
