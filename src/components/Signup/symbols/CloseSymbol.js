c1ed95d5fcb4cf6d2084854c5c075d52e0be18f0
mariotmc <mariotomicx@gmail.com>
2022-02-07 06:15:57 +0100

    Remove props, add state context

--- src/components/Signup/symbols/CloseSymbol.js
+++ src/components/Signup/symbols/CloseSymbol.js
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { StateContext } from "../../../App";
+
 export const CloseSymbol = (props) => {
+  const { setShowSignupModal } = useContext(StateContext);
+
   return (
     <div className="close-symbol-wrapper">
       <svg
@@ -8,7 +13,7 @@ export const CloseSymbol = (props) => {
         height={props.height}
         style={{ fill: "rgb(239, 243, 244)" }}
         viewBox="0 0 24 24"
-        onClick={() => props.setShowSignupModal(false)}
+        onClick={() => setShowSignupModal(false)}
       >
         <path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path>
       </svg>
