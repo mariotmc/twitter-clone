613e052ab7f5511dab573ba4e6b942ceb9b42dac
mariotmc <mariotomicx@gmail.com>
2022-02-07 06:19:09 +0100

    Move all states to App & create state context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import LoginScreen from "./components/LoginScreen/LoginScreen";
-import SignupModal from "./components/Signup/SignupModal";
+import Home from "./components/Home/Home";
+
+export const StateContext = createContext();
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+  const [page, setPage] = useState(1);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    month: "",
+    day: "",
+    year: "",
+  });
 
   return (
-    <>
-      {/* {!isLoggedIn ? <LoginScreen /> : <Home />} */}
-      {/* {!isLoggedIn && <LoginScreen />} */}
-      <SignupModal />
-    </>
+    <StateContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        showSignupModal,
+        setShowSignupModal,
+        formData,
+        setFormData,
+        page,
+        setPage,
+      }}
+    >
+      {!isLoggedIn ? <LoginScreen /> : <Home />}
+    </StateContext.Provider>
   );
 }
 
