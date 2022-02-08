aa3a903f3fa1a53e0ca38f71e4c7512c35f9cb79
mariotmc <mariotomicx@gmail.com>
2022-02-08 12:17:54 +0100

    Remove redundant pages

--- src/components/Signup/pages/SignupFifthPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignupFifthPage = () => {
-  return <div></div>;
-};
-
-export default SignupFifthPage;
--- src/components/Signup/pages/SignupFourthPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignupFourthPage = () => {
-  return <div></div>;
-};
-
-export default SignupFourthPage;
