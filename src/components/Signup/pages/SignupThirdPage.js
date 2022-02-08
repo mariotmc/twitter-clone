b6f27f4edb6e047b2ac04c657fc776dea3b1ab04
mariotmc <mariotomicx@gmail.com>
2022-02-08 12:23:27 +0100

    Add layout

--- src/components/Signup/pages/SignupThirdPage.js
+++ src/components/Signup/pages/SignupThirdPage.js
@@ -1,5 +1,47 @@
+import { ModalSecondaryHead } from "../utility/ModalSecondaryHead";
+import { FormNameInput } from "../input/FormNameInput";
+import { FormEmailInput } from "../input/FormEmailInput";
+import { DateOfBirthInput } from "../input/DateOfBirthInput";
+import { FormSignupButton } from "../buttons/FormSignupButton";
+
 const SignupThirdPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <ModalSecondaryHead />
+
+      <h3 id="signup-first-page-heading">Create your account</h3>
+
+      <form id="signup-form">
+        <FormNameInput />
+
+        <FormEmailInput />
+
+        <DateOfBirthInput />
+      </form>
+
+      <p id="signup-tos">
+        By signing up, you agree to our{" "}
+        <a href="https://twitter.com/en/tos" target="_blank" rel="noreferrer">
+          Terms
+        </a>
+        ,{" "}
+        <a href="https://twitter.com/en/privacy" target="_blank" rel="noreferrer">
+          Privacy Policy
+        </a>
+        , and{" "}
+        <a
+          href="https://help.twitter.com/en/rules-and-policies/twitter-cookies"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Cookie Use
+        </a>
+        .
+      </p>
+
+      <FormSignupButton />
+    </>
+  );
 };
 
 export default SignupThirdPage;
