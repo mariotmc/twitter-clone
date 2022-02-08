5d214406c4bb9f6f5f3c38bce28837f2d13b5f8f
mariotmc <mariotomicx@gmail.com>
2022-02-08 12:34:29 +0100

    Move form input provider & handleChange to App

--- src/components/Signup/SignupForm.js
+++ src/components/Signup/SignupForm.js
@@ -1,29 +1,17 @@
-import { createContext, useContext } from "react";
+import { useContext } from "react";
 import { StateContext } from "../../App";
 import SignupFirstPage from "./pages/SignupFirstPage";
 import SignupSecondPage from "./pages/SignupSecondPage";
 import SignupThirdPage from "./pages/SignupThirdPage";
-import SignupFourthPage from "./pages/SignupFourthPage";
-import SignupFifthPage from "./pages/SignupFifthPage";
-
-export const FormInputChangeContext = createContext();
 
 export const SignupForm = () => {
-  const { setFormData, page } = useContext(StateContext);
-
-  const handleChange = (e) => {
-    setFormData((prevState) => {
-      return { ...prevState, [e.target.name]: e.target.value };
-    });
-  };
+  const { page } = useContext(StateContext);
 
   return (
-    <FormInputChangeContext.Provider value={handleChange}>
+    <>
       {page === 1 && <SignupFirstPage />}
       {page === 2 && <SignupSecondPage />}
       {page === 3 && <SignupThirdPage />}
-      {page === 4 && <SignupFourthPage />}
-      {page === 5 && <SignupFifthPage />}
-    </FormInputChangeContext.Provider>
+    </>
   );
 };
