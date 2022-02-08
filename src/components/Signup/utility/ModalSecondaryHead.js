d2acb9a790c3286ffe676e48a2690dfa0e434cc8
mariotmc <mariotomicx@gmail.com>
2022-02-08 12:20:27 +0100

    Modify max page

--- src/components/Signup/utility/ModalSecondaryHead.js
+++ src/components/Signup/utility/ModalSecondaryHead.js
@@ -8,9 +8,9 @@ export const ModalSecondaryHead = () => {
   return (
     <div className="modal-secondary-head-container">
       <div style={{ display: "flex", textAlign: "center", marginRight: "22px" }}>
-        <ArrowLeftSymbol id="signup-arrow-left-symbol" width="20px" height="auto" />
+        <ArrowLeftSymbol id="signup-arrow-left-symbol" width="20px" height="34px" />
       </div>
-      <h3 id="signup-second-page-title">Step {page} of 5</h3>
+      <h3 id="signup-second-page-title">Step {page} of 3</h3>
     </div>
   );
 };
