ce047ae2d1a1429ec1fb1780dba0d4f8cb7a1042
mariotmc <mariotomicx@gmail.com>
2022-02-09 11:54:37 +0100

    Create first login page

---
+++ src/components/Signup/pages/LoginFirstPage.js
@@ -0,0 +1,32 @@
+import { ModalPrimaryHead } from "../utility/ModalPrimaryHead";
+import { GoogleButton } from "../../LoginScreen/buttons/GoogleButton";
+import { AppleButton } from "../../LoginScreen/buttons/AppleButton";
+import { Linebreak } from "../../LoginScreen/utility/Linebreak";
+import { SignInEmailInput } from "../input/SignInEmailInput";
+import { LoginNextButton } from "../buttons/LoginNextButton";
+import { SignUpCTA } from "../utility/SignUpCTA";
+
+export const LoginFirstPage = () => {
+  return (
+    <div id="login-modal-container" className="modal-container">
+      <div id="login-modal" className="modal">
+        <ModalPrimaryHead />
+        <form>
+          <h3>Sign in to Twitter</h3>
+
+          <GoogleButton id="google-icon" class="button signup-button" text="in" />
+
+          <AppleButton id="apple-icon" class="button signup-button" text="in" />
+
+          <Linebreak />
+
+          <SignInEmailInput />
+        </form>
+
+        <LoginNextButton />
+
+        <SignUpCTA />
+      </div>
+    </div>
+  );
+};
