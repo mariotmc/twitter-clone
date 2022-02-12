1493bf827c850c69d1ef422fa1d7fba5902eebfe
mariotmc <mariotomicx@gmail.com>
2022-02-12 13:18:48 +0100

    Add dynamic id assignment

--- src/components/Signup/utility/ModalPrimaryHead.js
+++ src/components/Signup/utility/ModalPrimaryHead.js
@@ -1,12 +1,12 @@
 import { CloseSymbol } from "../symbols/CloseSymbol";
 import { TwitterIcon } from "../../LoginScreen/utility/TwitterIcon";
 
-export const ModalPrimaryHead = () => {
+export const ModalPrimaryHead = ({ id }) => {
   return (
     <>
       <div id="modal-head-container">
         <div style={{ display: "flex", width: "270px" }}>
-          <CloseSymbol id="signup-close-symbol" width="20px" height="34px" />
+          <CloseSymbol id={id} width="20px" height="34px" />
         </div>
         <TwitterIcon class="very-small-icon white" />
         <div></div>
