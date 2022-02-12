39fe0a3e6b9a278d0fff71ef5942c88e56c89f81
mariotmc <mariotomicx@gmail.com>
2022-02-12 13:25:25 +0100

    Create refs for components

--- src/components/Signup/input/SignInEmailInput.js
+++ src/components/Signup/input/SignInEmailInput.js
@@ -2,46 +2,50 @@ import { useEffect } from "react";
 import { useAppContext } from "../../../Context";
 
 export const SignInEmailInput = ({ id }) => {
-  const { emailInputRef, loginData, handleChange, page } = useAppContext();
+  const { loginEmailRef, loginNextButton, loginData, handleChange, page, setInvalidBorderColor } =
+    useAppContext();
 
   const toggleButton = () => {
-    const loginButton = document.querySelector("#login-next-button");
-    if (loginButton)
-      !emailInputRef.current.value ? (loginButton.disabled = true) : (loginButton.disabled = false);
+    if (loginEmailRef && loginNextButton)
+      !loginEmailRef.current.value
+        ? (loginNextButton.current.disabled = true)
+        : (loginNextButton.current.disabled = false);
   };
 
   useEffect(() => {
-    const loginButton = document.querySelector("#login-next-button");
-    if (loginButton) {
-      if (page === 1 && !emailInputRef.current.value) loginButton.disabled = true;
-    }
-    if (page === 2) {
-      emailInputRef.current.disabled = true;
-      emailInputRef.current.style.color = "rgb(110, 118, 125)";
-      emailInputRef.current.parentElement.style.backgroundColor = "rgb(32, 35, 39)";
-      emailInputRef.current.parentElement.style.borderColor = "rgb(32, 35, 39)";
+    if (loginEmailRef && loginNextButton) {
+      if (page === 1 && !loginEmailRef.current.value) loginNextButton.current.disabled = true;
+      if (page === 2) {
+        loginEmailRef.current.disabled = true;
+        loginEmailRef.current.style.color = "rgb(110, 118, 125)";
+        loginEmailRef.current.parentElement.style.backgroundColor = "rgb(32, 35, 39)";
+        loginEmailRef.current.parentElement.style.borderColor = "rgb(32, 35, 39)";
+      }
     }
   }, []);
 
   return (
-    <div id={id} className="signup-input-container">
-      <input
-        id="signInEmail"
-        className="form-input"
-        type="email"
-        name="signInEmail"
-        placeholder="Email"
-        ref={emailInputRef}
-        onChange={(e) => {
-          handleChange(e);
-          toggleButton(e);
-        }}
-        value={loginData.signInEmail}
-        required
-      />
-      <label className="form-label" htmlFor="signInEmail">
-        Email
-      </label>
-    </div>
+    <>
+      <div id={id} className="signup-input-container">
+        <input
+          id="signInEmail"
+          className="form-input"
+          type="email"
+          name="signInEmail"
+          placeholder="Email"
+          ref={loginEmailRef}
+          onChange={(e) => {
+            handleChange(e);
+            toggleButton(e);
+          }}
+          onInvalid={() => setInvalidBorderColor(loginEmailRef)}
+          value={loginData.signInEmail}
+          required
+        />
+        <label className="form-label" htmlFor="signInEmail">
+          Email
+        </label>
+      </div>
+    </>
   );
 };
