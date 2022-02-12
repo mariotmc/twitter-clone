790f89f869f08795b854360e1e9345e4aef54ac4
mariotmc <mariotomicx@gmail.com>
2022-02-12 13:20:21 +0100

    Add conditional state resets

--- src/components/Signup/symbols/ArrowLeftSymbol.js
+++ src/components/Signup/symbols/ArrowLeftSymbol.js
@@ -1,7 +1,7 @@
 import { useAppContext } from "../../../Context";
 
 export const ArrowLeftSymbol = (props) => {
-  const { setPage } = useAppContext();
+  const { page, setPage, setPassword, setPasswordConfirmation, setLoginData } = useAppContext();
 
   const previousPage = () => {
     setPage((currentPage) => currentPage - 1);
@@ -16,7 +16,17 @@ export const ArrowLeftSymbol = (props) => {
         width={props.width}
         height={props.height}
         style={{ fill: "rgb(239, 243, 244)" }}
-        onClick={previousPage}
+        onClick={() => {
+          previousPage();
+          if (page === 2)
+            setLoginData((prevState) => {
+              return { ...prevState, signInPassword: "" };
+            });
+          if (page === 3) {
+            setPassword("");
+            setPasswordConfirmation("");
+          }
+        }}
       >
         <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path>
       </svg>
