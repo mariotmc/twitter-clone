8dd5108492a939a10ea9af3a4979972290acdb6a
mariotmc <mariotomicx@gmail.com>
2022-02-12 13:19:13 +0100

    Add reset form data after closing modal

--- src/components/Signup/symbols/CloseSymbol.js
+++ src/components/Signup/symbols/CloseSymbol.js
@@ -1,7 +1,7 @@
 import { useAppContext } from "../../../Context";
 
 export const CloseSymbol = (props) => {
-  const { setShowSignupModal } = useAppContext();
+  const { setShowSignupModal, setFormData, redirectUser } = useAppContext();
 
   return (
     <div className="close-symbol-wrapper">
@@ -12,7 +12,21 @@ export const CloseSymbol = (props) => {
         height={props.height}
         style={{ fill: "rgb(239, 243, 244)" }}
         viewBox="0 0 24 24"
-        onClick={() => setShowSignupModal(false)}
+        onClick={() => {
+          setShowSignupModal(false);
+          if (props.id === "signup-close-symbol")
+            setFormData({
+              name: "",
+              email: "",
+              month: "",
+              day: "",
+              year: "",
+              emailNotifications: false,
+              emailAccess: false,
+              personalizedAds: false,
+            });
+          if (props.id === "password-reset-close") redirectUser("login");
+        }}
       >
         <path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path>
       </svg>
