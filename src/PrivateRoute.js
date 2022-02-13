68e334bdefec844df2748afca8639267d4e11d43
mariotmc <mariotomicx@gmail.com>
2022-02-13 15:08:59 +0100

    Change loading condition

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,10 +1,8 @@
 import { Navigate } from "react-router-dom";
-import { useAppContext } from "./Context";
+import { auth } from "./firebase";
 
 const PrivateRoute = ({ children }) => {
-  const { currentUser } = useAppContext();
-
-  return currentUser ? children : <Navigate to="/twitter-clone/login" />;
+  return auth.currentUser !== undefined ? children : <Navigate to="/twitter-clone/login" />;
 };
 
 export default PrivateRoute;
