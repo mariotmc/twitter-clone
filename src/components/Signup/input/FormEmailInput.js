9a9c423e719b4670957f8761bc0b0865db1fcd19
mariotmc <mariotomicx@gmail.com>
2022-02-13 15:12:25 +0100

    Add error message

--- src/components/Signup/input/FormEmailInput.js
+++ src/components/Signup/input/FormEmailInput.js
@@ -1,25 +1,28 @@
 import { useAppContext } from "../../../Context";
 
 export const FormEmailInput = () => {
-  const { formData, handleChange, setInvalidBorderColor, signupEmailRef } = useAppContext();
+  const { formData, handleChange, setInvalidBorderColor, signupEmailRef, error } = useAppContext();
 
   return (
-    <div id="signup-email-input-container" className="signup-input-container">
-      <input
-        id="email"
-        className="form-input"
-        type="email"
-        name="email"
-        placeholder="Email"
-        required
-        ref={signupEmailRef}
-        onChange={handleChange}
-        onInvalid={() => setInvalidBorderColor(signupEmailRef)}
-        value={formData.email}
-      />
-      <label className="form-label" htmlFor="email">
-        Email
-      </label>
-    </div>
+    <>
+      <div id="signup-email-input-container" className="signup-input-container">
+        <input
+          id="email"
+          className="form-input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          ref={signupEmailRef}
+          onChange={handleChange}
+          onInvalid={() => setInvalidBorderColor(signupEmailRef)}
+          value={formData.email}
+        />
+        <label className="form-label" htmlFor="email">
+          Email
+        </label>
+      </div>
+      {error && error === "Email is already taken" && <p className="signup-error-message">{error}</p>}
+    </>
   );
 };
